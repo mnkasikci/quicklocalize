6fb4a02e159b6d0c7c39f4ae3e6c68d7b52453ff
Nurbaki Kasikci <baki.kasikci@grapelaw.com>
2026-04-19 11:30:52 +0200

    Add CI/CD workflow and Cloudflare Pages config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+
+if (process.env.NODE_ENV === 'development') {
+  setupDevPlatform().catch(console.error);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
