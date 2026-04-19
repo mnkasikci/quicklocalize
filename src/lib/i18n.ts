4c13e09ab2ba2f7f3e16bef50c234adc94d92b6d
Nurbaki Kasikci <baki.kasikci@grapelaw.com>
2026-04-19 15:47:58 +0200

    Add multiple language support to the list of supported languages in i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,6 +2,14 @@ export const SUPPORTED_LANGUAGES = [
   { code: 'en', name: 'English' },
   { code: 'es', name: 'Spanish' },
   { code: 'fr', name: 'French' },
+  {code: 'de', name: 'German'},
+  {code: 'it', name: 'Italian'},
+  {code: 'ja', name: 'Japanese'},
+  {code: 'ko', name: 'Korean'},
+  {code: 'pt', name: 'Portuguese'},
+  {code: 'ru', name: 'Russian'},
+  {code: 'zh', name: 'Chinese'},
+  {code: 'ar', name: 'Arabic'},
 ] as const;
 
 export type LanguageCode = (typeof SUPPORTED_LANGUAGES)[number]['code'];
